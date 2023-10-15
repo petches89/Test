49903ec2bd23ba54cf610a7c23ec2c9448dda9fd
Petronela Adamcova <petronela.adamcova@gmail.com>
2023-10-15 17:12:44 +0200

    first commit to the repository

---
+++ cypress/integration/examples/Test1.js
@@ -0,0 +1,14 @@
+/// <reference types="Cypress" />
+
+describe("My First Test Suite", function()
+    {
+
+    it("My First Test Case", function ()
+    {
+
+    cy.visit("https://betterask.erni/apptype-chooser/")
+    })
+
+
+    }
+)
